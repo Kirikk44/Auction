6c8d712e4ce947bb5e46741ca9a3e6b7b1011655
Kirikk44 <69201580+Kirikk44@users.noreply.github.com>
2022-03-22 00:19:54 +0300

    Add files via upload

---
+++ controllers/LotRequest.java
@@ -0,0 +1,10 @@
+package com.example.auction_test.models;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class LotRequest {
+    String name;
+    int startPrice;
+    LocalDateTime timeExistence;
+}
---
+++ controllers/LotResponse.java
@@ -0,0 +1,14 @@
+package com.example.auction_test.models;
+
+import java.time.LocalDateTime;
+
+public class LotResponse {
+    int currentBid; // == bets[0];
+    LocalDateTime start_time;
+    User owner;
+    int bidStep;
+    int lastBid;
+
+    Product product;
+
+}
